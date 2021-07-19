159d0218f5b8281dce0b50d198e1f160b3559a46
fabriciohenriquesilva <fabricionhsilva@gmail.com>
2021-07-18 22:03:53 -0300

    feat: remove item da lista

--- js/main.js
+++ js/main.js
@@ -5,15 +5,19 @@ let btoAdiciona = document.querySelector(".bto-adiciona");
 
 btoAdiciona.addEventListener("click", function() {
     let li = document.createElement("li");
+
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-
     li.appendChild(checkbox);
 
     let label = document.createElement("label");
     label.textContent = inputAdiciona.value;
-
     li.appendChild(label);
+
+    let img = document.createElement("img");
+    img.src = "../assets/icons/delete.png";
+    img.classList.add("remove-item");
+    li.appendChild(img);
 
     lista.appendChild(li);
     inputAdiciona.value = "";
@@ -25,11 +29,18 @@ lista.addEventListener("click", function(event) {
     if(event.target.parentNode.tagName == "LI"){
         let li = event.target.parentNode;
         // console.log(li);
+        if(event.target.tagName == "INPUT"){
+            let label = li.querySelector("label");
+            console.log(label.textContent);
+            label.classList.toggle("item-marcado");
+        }
+    }
+});
 
-        let label = li.querySelector("label");
-        console.log(label.textContent);
-
-        label.classList.toggle("item-marcado");
-        // let label = 
+lista.addEventListener("click", function(event) {
+    let li = event.target.parentNode;
+    if(event.target.tagName == "IMG"){
+        li.remove();
+        console.log(li);
     }
 });
